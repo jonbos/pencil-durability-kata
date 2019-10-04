688814325217b7676f597c4f3d14a2caefe7b95d
Jon Bos <bos.jon@gmail.com>
2019-10-04 13:34:30 -0500

    Adds test to ensure that new text is appended to existing text on paper

--- tests/test_pencil.py
+++ tests/test_pencil.py
@@ -1,10 +1,21 @@
 import unittest
-from pencil import Pencil 
+from pencil import Pencil
 from paper import Paper
+
+
 class PencilWritingTests(unittest.TestCase):
-	def test_should_write_text_to_paper(self):
-		text_to_write='abc'
-		pencil = Pencil()
-		paper= Paper()
-		pencil.write(text_to_write, paper)
-		self.assertEqual(paper.text, text_to_write)
+    def setUp(self):
+        self.pencil = Pencil()
+        self.paper = Paper()
+
+    def test_should_write_text_to_paper(self):
+        text_to_write = 'abc'
+        self.pencil.write(text_to_write, self.paper)
+        self.assertEqual(self.paper.text, text_to_write)
+
+    def test_should_append_text_to_end_of_paper(self):
+        self.paper.text='abc'
+        text_to_write='def'
+        self.pencil.write(text_to_write, self.paper)
+        self.assertEqual(self.paper.text, 'abcdef')
+        
