873ca00707c8010342a00e4ba7413791f67b5c99
Jon Bos <bos.jon@gmail.com>
2019-10-04 13:50:35 -0500

    A dull pencil only writes whitespace

--- pencil.py
+++ pencil.py
@@ -1,4 +1,18 @@
 class Pencil:
+
+    def __init__(self, point_durability=None):
+        self._point_durability = point_durability
+
+    @property
+    def point_durability(self):
+        return self._point_durability
+
+    @point_durability.setter
+    def point_durability(self,point_durability):
+        self._point_durability=point_durability
+
     def write(self, text, paper):
-    	paper.text+=text
-    	
+        if self.point_durability == 0:
+            paper.text += ' ' * len(text)
+        else:
+            paper.text += text
--- tests/test_pencil.py
+++ tests/test_pencil.py
@@ -14,8 +14,22 @@ class PencilWritingTests(unittest.TestCase):
         self.assertEqual(self.paper.text, text_to_write)
 
     def test_should_append_text_to_end_of_paper(self):
-        self.paper.text='abc'
-        text_to_write='def'
+        self.paper.text = 'abc'
+        text_to_write = 'def'
         self.pencil.write(text_to_write, self.paper)
         self.assertEqual(self.paper.text, 'abcdef')
-        
+
+
+class PointDegradationTests(unittest.TestCase):
+    def setUp(self):
+        self.paper = Paper()
+
+    def test_can_create_pencil_with_point_durability(self):
+        pencil = Pencil(point_durability=0)
+        self.assertEqual(pencil.point_durability, 0)
+
+    def test_should_write_whitespace_if_point_durability_is_zero(self):
+        pencil = Pencil(point_durability=0)
+        text_to_write = 'abc'
+        pencil.write(text_to_write, self.paper)
+        self.assertEqual(self.paper.text, ' ' * len(text_to_write))
