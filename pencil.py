ff0079b4a642ec38801ba49b6ff93cf2f3d015c2
Jon Bos <bos.jon@gmail.com>
2019-10-04 14:38:57 -0500

    Passing test for degrading point when writing a string of characters. Moved writing logic to private method that writes a single character at a time.

--- pencil.py
+++ pencil.py
@@ -8,12 +8,19 @@ class Pencil:
         return self._point_durability
 
     @point_durability.setter
-    def point_durability(self,point_durability):
-        self._point_durability=point_durability
+    def point_durability(self, point_durability):
+        self._point_durability = point_durability
 
     def write(self, text, paper):
-        if self.point_durability == 0:
-            paper.text += ' ' * len(text)
+        for char in text:
+            self._write_char(char, paper)
+
+    def _write_char(self, char, paper):
+        if self.point_durability==0:
+            paper.text+=' '
         else:
-            self.point_durability-=1
-            paper.text += text
+            if char.isupper():
+                self.point_durability-=2
+            elif char.islower():
+                self.point_durability-=1
+            paper.text+=char
--- tests/test_pencil.py
+++ tests/test_pencil.py
@@ -24,6 +24,7 @@ class PointDegradationTests(unittest.TestCase):
 
     def setUp(self):
         self.paper = Paper()
+
     def test_can_create_pencil_with_point_durability(self):
         pencil = Pencil(point_durability=0)
         self.assertEqual(pencil.point_durability, 0)
@@ -34,8 +35,32 @@ class PointDegradationTests(unittest.TestCase):
         pencil.write(text_to_write, self.paper)
         self.assertEqual(self.paper.text, ' ' * len(text_to_write))
 
-    def test_lowercase_letters_degrade_pencil_by_one_unit(self):
-        durability=1
-        pencil=Pencil(point_durability=durability)
+    def test_should_degrade_point_by_one_unit_when_writing_lowercase_char(self):
+        durability = 1
+        pencil = Pencil(point_durability=durability)
         pencil.write('a', self.paper)
-        self.assertEqual(pencil.point_durability, durability-1)
+        self.assertEqual(pencil.point_durability, durability - 1)
+
+    def test_should_degrade_point_by_two_units_when_writing_uppercase_char(self):
+        durability = 2
+        pencil = Pencil(point_durability=durability)
+        pencil.write('A', self.paper)
+        self.assertEqual(pencil.point_durability, durability - 2)
+
+    def test_should_not_degrade_point_when_writing_whitespace(self):
+        durability = 1
+        pencil = Pencil(point_durability=durability)
+        pencil.write(' ', self.paper)
+        self.assertEqual(pencil.point_durability, durability)
+
+    def test_should_degrade_pencil_by_sum_of_characters_cost_units(self):
+        #   2 cost units *2 uppercase chars
+        # + 1 cost unit * 4 lowercase chars  
+        # + 0 cost units * 1 space
+        # =8 cost units
+
+        to_write = 'Abc Def'
+        durability=8
+        pencil=Pencil(point_durability=8)
+        pencil.write(to_write, self.paper)
+        self.assertEqual(pencil.point_durability,durability-8)
