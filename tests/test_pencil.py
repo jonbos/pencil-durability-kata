d12066961d2abd0a2a35be39f98f771d0db51d76
Jon Bos <bos.jon@gmail.com>
2019-10-05 13:23:57 -0500

    Adds pencil sharpening and associated tests

--- tests/test_pencil.py
+++ tests/test_pencil.py
@@ -2,7 +2,6 @@ import unittest
 from pencil import Pencil
 from paper import Paper
 
-
 class PencilWritingTests(unittest.TestCase):
     def setUp(self):
         self.pencil = Pencil()
@@ -18,7 +17,6 @@ class PencilWritingTests(unittest.TestCase):
         text_to_write = 'def'
         self.pencil.write(text_to_write, self.paper)
         self.assertEqual(self.paper.text, 'abcdef')
-
 
 class PointDegradationTests(unittest.TestCase):
 
