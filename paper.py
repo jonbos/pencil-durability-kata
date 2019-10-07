180fff40b4ac63664b3bb45f39de9c61d8b157d6
Jon Bos <bos.jon@gmail.com>
2019-10-07 08:36:11 -0500

    Make paper fields public and remove unnecessary getters and setters

--- paper.py
+++ paper.py
@@ -1,12 +1,4 @@
 class Paper:
     def __init__(self):
-        self._text = ''
+        self.text = ''
         self.last_erased=-1
-
-    @property
-    def text(self):
-        return str(self._text)
-
-    @text.setter
-    def text(self, text):
-        self._text = text
