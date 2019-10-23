f5669ae2173325ddd5b09272dfced8a5d1e1f72e
Jon Bos <bos.jon@gmail.com>
2019-10-22 21:52:08 -0500

    Moves test_pencil_factory away from unittest (and toward pytest in the future)

--- tests/test_pencil_factory.py
+++ tests/test_pencil_factory.py
@@ -1,13 +1,8 @@
-import unittest
 from src import pencil_factory
 
+def test_should_return_number_2hb_pencil():
+    pencil = pencil_factory.get_no2_hb()
 
-class Number2HBPencilFactoryTests(unittest.TestCase):
-    def test_should_return_number_2hb_pencil(self):
-        pencil = pencil_factory.get_no2_hb()
-
-        self.assertEqual(pencil.point_durability,
-                         pencil_factory.NO2_HB_POINT_DURABILITY)
-        self.assertEqual(pencil.length, pencil_factory.NO2_HB_LENGTH)
-        self.assertEqual(pencil.eraser.durability,
-                         pencil_factory.NO2_HB_ERASER_DURABILITY)
+    assert pencil.point_durability == pencil_factory.NO2_HB_POINT_DURABILITY
+    assert pencil.length == pencil_factory.NO2_HB_LENGTH
+    assert pencil.eraser.durability == pencil_factory.NO2_HB_ERASER_DURABILITY
